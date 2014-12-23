82f4dad6b9287eab77263f8d5c1ff053195a6600
Ewout Quax <ewout.quax@quicknet.nl>
2014-12-24 00:41:35 +0100

    Install and configure simplecoverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter do |source_file|
+    source_file.filename.match(/lib/).nil?
+  end
+end
+
 require 'pry'
 
 require File.expand_path('../../lib/objection.rb', __FILE__)
